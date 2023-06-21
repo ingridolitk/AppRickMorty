80b21ca1f6d166ea62011b008a382bfa00605c3a
ingridolitk <ingrid.oliveiratk@gmail.com>
2023-06-21 00:29:53 -0300

    ajuste de código details

--- app/src/main/java/com/example/myapplication/domain/repository/DetailsCharactersRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.domain.repository
-
-import com.example.myapplication.domain.model.Result
-
-
-interface DetailsCharactersRepository {
-    suspend fun getDetailsCharacters(): Result
-}
--- app/src/main/java/com/example/myapplication/utils/DetailsCharacterState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.utils
-
-import com.example.myapplication.domain.model.Result
-
-sealed class DetailsCharacterState {
-    data class Loading(val isLoading: Boolean) : DetailsCharacterState()
-    data class ResponseData(val details: Result) : DetailsCharacterState()
-    data class Error(val error: String?) : DetailsCharacterState()
-}
